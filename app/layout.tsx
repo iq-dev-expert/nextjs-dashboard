7119716c5dac61b92c97f2c20261f7ba2f7f8717
Oleksandr Rakomin <uamilliardua@gmail.com>
2023-12-11 12:36:29 +0100

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { inter } from '@/app/ui/fonts';
 import { Metadata } from 'next';
+import { Analytics } from '@vercel/analytics/react';
 
 import '@/app/ui/global.css';
 
@@ -19,7 +20,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
